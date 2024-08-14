d692fd54ef318eeccb3378be6ac975be733fbd8b
PROF ROMULO <rjlpena@gmail.com>
2024-08-14 19:06:12 -0300

    R8

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
@@ -60,11 +61,14 @@ function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener('click', function(){
-        atual++
-        mostraPergunta();
-        })
+        botaoAlternativas.addEventListener('click', ()=> respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);        
     }
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal = afirmacao;
+    atual++
+    mostraPergunta(); 
 }
 mostraPergunta();
